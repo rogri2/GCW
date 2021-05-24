60d435181ed88c54233debe425260fc5d77ca7b9
rogri2 <rodyap182@gmail.com>
2021-05-24 15:18:31 -0500

    Compartir en FB
    Se agregó para poder compartir en facebook

---
+++ libs/facebook.js
@@ -0,0 +1,26 @@
+window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '140596344553990',
+      xfbml      : true,
+      version    : 'v2.9'
+    });
+    FB.AppEvents.logPageView();
+  };
+  
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk')); 
+  
+  
+  function share() {
+    FB.ui({
+      method: 'share',
+      href: 'https://google.com',
+      hashtag: '#STABCDMA',
+      quote: "Estoy jugando Super Turbo Armored Battle-Cars Duel Mini Arena"
+    }, function(response){});
+  }
