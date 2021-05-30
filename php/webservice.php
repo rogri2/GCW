3d7026321ea9b278e8c855481e6b7cd06dd38457
rogri2 <rodyap182@gmail.com>
2021-05-30 16:33:56 -0500

    Mostrar ganadores
    Se implementó el mostrar ganadores en el scoreboard

--- php/webservice.php
+++ php/webservice.php
@@ -1,4 +1,11 @@
 <?php
+	$action = $_POST['action'];
+
+	if ($action == "Winner") 
+		Winner();
+	else if ($action == "getWinners")
+		getWinners();
+
     function connect() {
 		$databasehost = "localhost";
 		$databaseuser = "root";
@@ -27,4 +34,21 @@
 		mysqli_close($mysqli);
 	}
 
-    Winner();
+	function getWinners() {
+		$mysqli = connect();
+
+		$result = $mysqli->query("SELECT * FROM Winners");	
+		
+		if (!$result) {
+			echo "Problema al hacer un query: " . $mysqli->error;								
+		} else {
+			// Recorremos los resultados devueltos
+			$rows = array();
+			while( $r = $result->fetch_assoc()) {
+				$rows[] = $r;
+			}			
+			// Codificamos los resultados a formato JSON y lo enviamos al HTML (Client-Side)
+			echo json_encode($rows);
+		}
+		mysqli_close($mysqli);
+	}
